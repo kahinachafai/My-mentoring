fdb354d12d6ba7a27b84d91d128463411666a8d5
kahina <kahinchafai@gmail.com>
2020-04-28 13:26:05 -0400

    added files

---
+++ src/mentoring/Add.java
@@ -0,0 +1,20 @@
+package mentoring;
+
+import java.util.Scanner;
+
+public class Add {
+
+    public static void main(String[] args) {
+
+        int number;
+        Scanner input= new Scanner(System.in);
+
+        System.out.println(" Enter your number");
+
+        number =input.nextInt();
+
+
+    }
+
+
+}
